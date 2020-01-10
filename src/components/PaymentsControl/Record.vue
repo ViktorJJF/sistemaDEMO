82016efb6f914405f34b7fda5599ffffcee8fccd
Victor Juan Jimenez Flores <vj.jimenez96@gmail.com>
2020-01-09 22:55:35 -0500

    added payment control

---
+++ src/components/PaymentsControl/Record.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-container fluid>
+    <v-dialog v-model="dialog" width="800">
+      <template v-slot:activator="{ on }">
+        <v-btn class="text-none" small color="info" dark v-on="on">{{text}}</v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title class="headline lighten-2" primary-title>Historial de pagos</v-card-title>
+        <v-divider></v-divider>
+        <v-container fluid class="mx-4">
+          <span class="body-1 font-weight-bold mb-0">Nombres:</span>
+          <span>Victor juan</span>
+          <v-simple-table>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th
+                    v-for="payment in payments"
+                    :key="payment.id"
+                    class="text-left"
+                  >{{payment.name}}</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td v-for="payment in payments" :key="payment.name">
+                    <v-checkbox color="info" v-model="payment.paid" :value="payment.paid"></v-checkbox>
+                  </td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </v-container>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="info" text @click="dialog = false">{{Text}}</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String
+  },
+  data() {
+    return {
+      dialog: false,
+      payments: [
+        { id: 1, name: "Enero", paid: false },
+        { id: 2, name: "Febrero", paid: true },
+        { id: 3, name: "Marzo", paid: true },
+        { id: 4, name: "Abril", paid: true },
+        { id: 5, name: "Mayo", paid: true },
+        { id: 6, name: "Junio", paid: true },
+        { id: 7, name: "Julio", paid: false },
+        { id: 8, name: "Agosto", paid: true },
+        { id: 9, name: "Septiembre", paid: false },
+        { id: 10, name: "Octubre", paid: true },
+        { id: 11, name: "Noviembre", paid: false },
+        { id: 12, name: "Diciembre", paid: true }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/PaymentsControl.vue
+++ src/views/PaymentsControl.vue
@@ -1,20 +1,436 @@
 <template>
-  <material-card
-    width="800px"
-    icon="mdi-account-card-details"
-    color="primary"
-    title="Control de pagos"
-    text="Servicio vigente"
-  ></material-card>
+  <v-container fluid>
+    <v-row justify="center">
+      <material-card
+        width="95%"
+        icon="mdi-account-card-details"
+        color="primary"
+        title="Control de pagos"
+        text="Resumen de control de pagos"
+      >
+        <v-select
+          dark
+          background-color="primary"
+          placeholder="Selecciona rango"
+          item-text="first_name"
+          dense
+          :items="$store.state.membersModule.members"
+          item-value="_id"
+          outlined
+        ></v-select>
+        <v-data-table
+          :loading="dataTableLoading"
+          loading-text="Cargando datos"
+          no-results-text="No se encontraron resultados"
+          :search="search"
+          hide-default-footer
+          :headers="headers"
+          :items="members"
+          sort-by="calories"
+          @page-count="pageCount = $event"
+          :page.sync="page"
+          :items-per-page="itemsPerPage"
+        >
+          <template v-slot:top>
+            <v-container>
+              <span class="font-weight-bold">Filtrar por nombre: {{ search }}</span>
+              <v-row>
+                <v-col cols="12" sm="6">
+                  <v-text-field
+                    clearable
+                    dense
+                    hide-details
+                    v-model="search"
+                    append-icon="mdi-magnify"
+                    placeholder="Escribe el nombre a buscar"
+                    single-line
+                    outlined
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-dialog v-model="dialog" max-width="900px">
+                    <template v-slot:activator="{ on }">
+                      <v-btn color="info" dark class="mb-2 text-none" v-on="on">Agregar Miembro</v-btn>
+                    </template>
+                    <v-card>
+                      <v-card-title>
+                        <span class="headline">{{ formTitle }}</span>
+                      </v-card-title>
+                      <v-divider></v-divider>
+                      <v-container class="pa-5">
+                        <v-row dense>
+                          <v-col cols="12" sm="4">
+                            <v-card>
+                              <v-container fluid>
+                                <image-uploader :img="editedItem.profile_picture"></image-uploader>
+                                <b class="center">
+                                  {{ editedItem.first_name }}
+                                  {{ editedItem.last_name }}
+                                </b>
+                                <v-select
+                                  dark
+                                  background-color="primary"
+                                  placeholder="Selecciona rango"
+                                  item-text="name"
+                                  dense
+                                  v-model="editedItem.rank"
+                                  :items="ranks"
+                                  item-value="_id"
+                                  outlined
+                                ></v-select>
+                              </v-container>
+                            </v-card>
+                          </v-col>
+                          <v-col cols="12" sm="8">
+                            <v-card>
+                              <v-container fluid>
+                                <h3>Detalles del usuario</h3>
+                                <v-row dense>
+                                  <v-col cols="12" sm="6" md="6">
+                                    <p class="body-1 font-weight-bold mb-0">Nombres</p>
+                                    <v-text-field
+                                      dense
+                                      clearable
+                                      class
+                                      outlined
+                                      v-model="editedItem.first_name"
+                                      placeholder="Nombres"
+                                    ></v-text-field>
+                                  </v-col>
+                                  <v-col cols="12" sm="6" md="6">
+                                    <p class="body-1 font-weight-bold mb-0">Apellidos</p>
+                                    <v-text-field
+                                      dense
+                                      clearable
+                                      class
+                                      outlined
+                                      v-model="editedItem.last_name"
+                                      placeholder="Apellidos"
+                                    ></v-text-field>
+                                  </v-col>
+                                  <v-col cols="12" sm="12" md="12">
+                                    <p class="body-1 font-weight-bold mb-0">Correo</p>
+                                    <v-text-field
+                                      dense
+                                      clearable
+                                      class
+                                      outlined
+                                      v-model="editedItem.email"
+                                      placeholder="Correo"
+                                    ></v-text-field>
+                                  </v-col>
+                                  <v-col cols="12" sm="6" md="6">
+                                    <p class="body-1 font-weight-bold mb-0">Teléfono</p>
+                                    <v-text-field
+                                      dense
+                                      clearable
+                                      class
+                                      outlined
+                                      v-model="editedItem.cell_number"
+                                      placeholder="+33 680 807 5770"
+                                    ></v-text-field>
+                                  </v-col>
+                                  <v-col cols="12" sm="6" md="6">
+                                    <p class="body-1 font-weight-bold mb-0">Celular</p>
+                                    <v-text-field
+                                      dense
+                                      clearable
+                                      class
+                                      outlined
+                                      v-model="editedItem.phone_number"
+                                      placeholder="(800) 3537342"
+                                    ></v-text-field>
+                                  </v-col>
+                                  <v-col cols="12" sm="6" md="6">
+                                    <p class="body-1 font-weight-bold mb-0">País</p>
+                                    <v-select
+                                      placeholder="Selecciona rango"
+                                      item-text="name"
+                                      dense
+                                      v-model="editedItem.country"
+                                      :items="countries"
+                                      item-value="_id"
+                                      outlined
+                                    ></v-select>
+                                  </v-col>
+                                  <v-col cols="12" sm="6" md="6">
+                                    <p class="body-1 font-weight-bold mb-0">Ciudad</p>
+                                    <v-text-field
+                                      dense
+                                      clearable
+                                      class
+                                      outlined
+                                      v-model="editedItem.city"
+                                      placeholder="Selecciona una ciudad"
+                                    ></v-text-field>
+                                  </v-col>
+                                  <v-col cols="12" sm="12" md="12">
+                                    <p class="body-1 font-weight-bold mb-0">Dirección</p>
+                                    <v-text-field
+                                      dense
+                                      clearable
+                                      class
+                                      outlined
+                                      v-model="editedItem.address"
+                                      placeholder="059 2nd Hill"
+                                    ></v-text-field>
+                                  </v-col>
+                                </v-row>
+                              </v-container>
+                            </v-card>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                      <v-card-actions>
+                        <div class="flex-grow-1"></div>
+                        <v-btn outlined color="error" text @click="close">Cancelar</v-btn>
+                        <v-btn :loading="loadingButton" color="success" @click="save">Guardar</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+                </v-col>
+              </v-row>
+              <v-row justify="end">
+                <v-btn small color="info" class="mr-3" @click="exportPDF">Exportar a PDF</v-btn>
+                <v-btn small color="info" @click="exportExcel">Exportar a Excel</v-btn>
+              </v-row>
+            </v-container>
+          </template>
+          <template v-slot:item.paymentState>
+            <v-chip color="error">
+              <v-avatar left>
+                <v-icon>mdi-close-circle</v-icon>
+              </v-avatar>Falta pagar
+            </v-chip>
+            <v-chip color="success">
+              <v-avatar left>
+                <v-icon>mdi-checkbox-marked-circle</v-icon>
+              </v-avatar>Pagó
+            </v-chip>
+          </template>
+          <template v-slot:item.action="{ item }">
+            <v-btn
+              class="mr-3 mb-1 text-none"
+              small
+              color="success"
+              @click="editItem(item)"
+            >Agregar pago</v-btn>
+            <record text="Ver informe"></record>
+          </template>
+          <template v-slot:no-data>
+            <v-alert type="error" :value="true">Aún no cuentas con marcas de productos</v-alert>
+          </template>
+          <template v-slot:item.createdAt="{ item }">{{ item.createdAt | dateFormat }}</template>
+          <template v-slot:item.profile_picture="{ item }">
+            <v-img
+              :src="item.profile_picture"
+              class="border-image"
+              contain
+              max-width="200"
+              max-height="150"
+            ></v-img>
+          </template>
+        </v-data-table>
+        <v-col cols="12" sm="12">
+          <span>
+            <strong>Total de miembros:</strong>
+            {{ members.length }}
+          </span>
+        </v-col>
+        <div class="text-center pt-2">
+          <v-pagination v-model="page" :length="pageCount"></v-pagination>
+        </div>
+      </material-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import dateFormat from "@/utils/customDate";
+import Member from "@/classes/Member.js";
+import { customCopyObject } from "@/utils/customCopyObject";
+import { customHttpRequest } from "@/utils/customHttpRequest";
 import MaterialCard from "@/components/global/MaterialCard";
+import XLSX from "xlsx";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+import ImageUploader from "@/components/common/ImageUploader";
+import Record from "@/components/PaymentsControl/Record";
+
 export default {
   components: {
-    MaterialCard
+    MaterialCard,
+    ImageUploader,
+    Record
+  },
+  filters: {
+    dateFormat: function(value) {
+      return dateFormat(value);
+    }
+  },
+  data: () => ({
+    dataTableLoading: true,
+    page: 1,
+    pageCount: 0,
+    itemsPerPage: 5,
+    loadingButton: false,
+    validateError: false,
+    search: "",
+    dialog: false,
+    headers: [
+      {
+        text: "DNI",
+        align: "left",
+        sortable: false,
+        value: "dni",
+        class: "customHeader"
+      },
+      {
+        text: "Perfil",
+        align: "left",
+        sortable: false,
+        value: "profile_picture",
+        class: "customHeader"
+      },
+      {
+        text: "Nombres",
+        align: "left",
+        sortable: true,
+        value: "first_name",
+        class: "colored",
+        width: 150
+      },
+      {
+        text: "Apellidos",
+        align: "left",
+        sortable: true,
+        value: "last_name",
+        width: 150
+      },
+      {
+        text: "Estado de pagos",
+        align: "left",
+        sortable: true,
+        value: "paymentState",
+        width: 150
+      },
+      { text: "Acciones", value: "action", sortable: false }
+    ],
+    // members: [],
+    editedIndex: -1,
+    editedItem: {},
+    defaultItem: {}
+  }),
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "Nuevo pago" : "Resumen de pagos";
+    },
+    members() {
+      return this.$store.state.membersModule.members;
+    }
+  },
+  watch: {
+    dialog(val) {
+      val || this.close();
+    }
+  },
+  mounted() {
+    this.initialData();
+  },
+  methods: {
+    async initialData() {
+      this.dataTableLoading = true;
+      await this.$store.dispatch("membersModule/fetchMembers");
+      this.dataTableLoading = false;
+    },
+    exportExcel() {
+      let data = XLSX.utils.json_to_sheet(this.members);
+      const workbook = XLSX.utils.book_new();
+      const filename = "miembros-logia";
+      XLSX.utils.book_append_sheet(workbook, data, filename);
+      XLSX.writeFile(workbook, `${filename}.xlsx`);
+    },
+    exportPDF() {
+      // Default export is a4 paper, portrait, using milimeters for units
+      var doc = new jsPDF({
+        orientation: "landscape"
+      });
+      doc.text(
+        "Listado de miembros de la logia: Francisco de Paula Gonzáles Vigil N° 38",
+        15,
+        10
+      );
+      doc.autoTable({
+        theme: "grid",
+        headStyles: { fillColor: [25, 53, 93] },
+        styles: { fontSize: 9 },
+        columnStyles: { europe: { halign: "center" } }, // European countries centered
+        body: this.members,
+        columns: [
+          { header: "DNI", dataKey: "dni" },
+          { header: "Nombres", dataKey: "first_name" },
+          { header: "Apellidos", dataKey: "last_name" },
+          { header: "Correo", dataKey: "email" },
+          { header: "Celular", dataKey: "cell_number" },
+          { header: "Teléfono", dataKey: "phone_number" },
+          { header: "Dirección", dataKey: "address" },
+          { header: "País", dataKey: "country" },
+          { header: "Ciudad", dataKey: "city" },
+          { header: "Rango", dataKey: "rank" }
+        ]
+      });
+      doc.save("reporte_miembros_logia.pdf");
+    },
+    editItem(item) {
+      this.editedIndex = this.members.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+    async deleteItem(item) {
+      const index = this.members.indexOf(item);
+      let membersId = this.members[index]._id;
+      const confirm = await this.$confirm(
+        "¿Realmente deseas eliminar este registro?"
+      );
+      if (confirm) {
+        this.$store.dispatch("membersModule/deleteMember");
+        this.members.splice(index, 1);
+      }
+    },
+    close() {
+      this.dialog = false;
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      }, 300);
+    },
+    save() {
+      if (this.editedIndex > -1) {
+        //update member
+        this.$store.dispatch("membersModule/editMember", {
+          id: this.members[this.editedIndex]._id,
+          data: this.editedItem
+        });
+        this.close();
+      } else {
+        //create member
+        this.members.push(this.editedItem);
+        this.close();
+      }
+    }
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.border-image {
+  border-style: solid;
+  border-width: 1px;
+}
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+}
+</style>
