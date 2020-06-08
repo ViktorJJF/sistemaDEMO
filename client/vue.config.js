4a391d567569a95280846f52157b064dfc0b527a
Victor Juan Jimenez Flores <vj.jimenez96@gmail.com>
2020-06-08 18:21:51 -0500

    testing axios baseUrl

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,13 +1,20 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    outputDir: path.resolve(__dirname, '../server/public'),
-    devServer: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3000'
-                    // target: 'https://mastermindgeeks.herokuapp.com'
-            }
-        }
-    }
+  outputDir: path.resolve(__dirname, "../server/public"),
+
+  devServer: {
+    proxy: {
+      "/api": {
+        // target: 'http://localhost:3000'
+        target: "https://b50b0518c9ed.ngrok.io/",
+      },
+    },
+  },
+
+  publicPath: '',
+
+  pluginOptions: {
+    cordovaPath: 'MasonCordova',
+  },
 };
