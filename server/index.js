444fe869b0459b622f327331f9dfa52694ac4324
Victor Juan Jimenez Flores <vj.jimenez96@gmail.com>
2020-06-09 01:53:35 -0500

    testing sessions

--- server/index.js
+++ server/index.js
@@ -68,7 +68,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://localhost:8080");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept"
