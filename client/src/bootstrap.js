4ee3d0dffbd81f3bf890f0774155c9ddebd25b0e
Victor Juan Jimenez Flores <vj.jimenez96@gmail.com>
2020-06-08 22:25:30 -0500

    testing cors

--- client/src/bootstrap.js
+++ client/src/bootstrap.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 window.axios = axios;
 window.axios.defaults.baseURL = "https://logia-demo.herokuapp.com";
+// window.axios.defaults.withCredentials = true;
+window.axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 window.axios.defaults.timeout = 10000;
 
 // window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
